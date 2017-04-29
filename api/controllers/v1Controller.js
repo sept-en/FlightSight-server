d95f21cbf2142f01076558dc3e2b1e5e8d966047
Karim <karimkaspersky@yahoo.com>
2017-04-29 19:39:31 +0300

    implement API : get route places by lat & lng

--- api/controllers/v1Controller.js
+++ api/controllers/v1Controller.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function point_at_distance(x0, y0, x1, y1, ratio) {
+   return {lat:((1 - ratio) * x0 + ratio * x1), lng: ((1 - ratio) * y0 + ratio * y1)};
+}
+
 exports.testAPI = function(req, res) {
   console.log('testAPI: ' + req.params.name);
   res.json("{'p1' : 'hi " + req.params.name + "'}");
@@ -7,21 +11,70 @@ exports.testAPI = function(req, res) {
 
 exports.route_places_by_latlng = function(req, res) {
   console.log('route_places_by_latlng: ');
-  res.json("{'api' : 'route_places_by_latlng'}");
-};
 
-//route_places/by_latlng/:id/:start_lat/:start_lng/:finish_lat/:finish_lng
-// {
-//     request-id: 123;
-//     objects: [
-//     {
-//         latitude: ...;
-//         longtitude: ....;
-//         name: Barcelone;
-//         description:  ....;
-//         path-progress: ...; # path progress of current location in %
-//         images: [ "https://...", "https://...."]
-//     },
-//     {  .... }
-//     ]
-// }
+  var start_lat = req.params.start_lat;
+  var start_lng = req.params.start_lng;
+  var finish_lat = req.params.finish_lat;
+  var finish_lng = req.params.finish_lng;
+  
+  var points = [];
+  var ratio_of_distances = 0.0;
+
+  while (ratio_of_distances <= 1)
+  {
+    var point_at = point_at_distance(start_lat, start_lng, finish_lat, finish_lng, ratio_of_distances);
+    point_at.ratio = ratio_of_distances;
+    points.push(point_at);
+    ratio_of_distances += 0.1;
+  }
+
+  // points.push({lat:51.503186, lng:-0.126446, ratio:0.1});
+  // console.log(points);
+
+  var request = require("request");
+  var resJson = {};
+  resJson.request_id = req.params.id;
+  var objects = [];
+
+  var place_index = 0;
+  var request_count = 0;
+  for (var point of points)
+  {
+    var location_info_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${point.lat},${point.lng}&radius=500&type=restaurant&keyword=cruise&key=${global.googleAPI}`;
+
+    ++request_count;
+    request(location_info_URL, function(error, response, body) {
+      var results = JSON.parse(body).results;
+      if (results.length > 0)
+      {
+        for (var place of results)
+        {
+           var res_place = {index       : place_index,
+                            location    : place.geometry.location,//lat,lng
+                            icon        : place.icon,
+                            name        : place.name, 
+                            description : "Kill me",
+                            images      : [],
+                            path_progress : point.ratio};
+
+           for (var photo of place.photos)
+           {
+              var photo_URL =`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=${global.googleAPI}`;
+              res_place.images.push(photo_URL);
+           } 
+
+           
+           objects.push (res_place);
+        }
+        
+        place_index++;
+      }
+
+      if(--request_count == 0)
+      {
+        resJson.objects = objects;
+        res.json(resJson);
+      }
+    }); 
+  }
+};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
   port = process.env.PORT || 8181,
   bodyParser = require('body-parser');
 
-
+global.googleAPI = "AIzaSyBHzy6L_1NuePNhZDcYGd0gS5OsQLf5Ch0";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
